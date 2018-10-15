7b87fecca02e41f9fa503114928ba985a41c8435
luismurao <luismurao@gmail.com>
2018-10-14 19:32:40 -0500

    V 0.2.5 Changed “.” for “_”

--- R/extract_by_year.R
+++ R/extract_by_year.R
@@ -1,12 +1,13 @@
 #' Extract environmental data by year.
 #' @description Extract environmental data by year.
-#' @param this.species, Species Temporal Data object see \code{\link[hdm]{sp_temporal_data}}.
+#' @param this_species, Species Temporal Data object see \code{\link[hsi]{sp_temporal_data}}.
 #' @param layers_pattern A regular expression to indicate the pattern to search in layers files names.
+#' @import purrr
 #' @export
-extract_by_year <- function(this.species,layers_pattern){
-  stopifnot(inherits(this.species, "sp.temporal.modeling"))
-  years <- sort(unique(this.species$sp_occs_year))
-  capasByRes <- lapply(this.species$layers_path_by_year,
+extract_by_year <- function(this_species,layers_pattern){
+  stopifnot(inherits(this_species, "sp.temporal.modeling"))
+  years <- sort(unique(this_species$sp_occs_year))
+  capasByRes <- lapply(this_species$layers_path_by_year,
                        function(x){
                          x[grep(layers_pattern,x = x)]
                        })
@@ -16,13 +17,13 @@ extract_by_year <- function(this.species,layers_pattern){
     year_env <- x$years_in_occs[1]
     env_layers <- raster::stack(capasByRes[[paste0(year_env)]])
     env_sp <- raster::extract(x = env_layers,
-                      y = x[,this.species$lon_lat_vars],
+                      y = x[,this_species$lon_lat_vars],
                       df=TRUE)
 
     names(env_sp) <- c("ID_YEAR",
                        names(env_sp)[2:length(names(env_sp))])
-    coords.x1 <- x[,this.species$lon_lat_vars[1]]
-    coords.x2 <- x[,this.species$lon_lat_vars[2]]
+    coords.x1 <- x[,this_species$lon_lat_vars[1]]
+    coords.x2 <- x[,this_species$lon_lat_vars[2]]
 
     env_sp$ID_YEAR <- year_env
     n_70 <- floor(dim(env_sp)[1]*.7)
@@ -33,13 +34,13 @@ extract_by_year <- function(this.species,layers_pattern){
     return(cbind(coords.x1,coords.x2,env_sp))
   }
 
-  df_occs_year <- this.species$oocs_data
+  df_occs_year <- this_species$oocs_data
   years_env <- df_occs_year %>%
     split(.$years_in_occs) %>%
     purrr::map_df(~years_env_fun(.x))
 
   train_data <- which(years_env$trian_test=="Train")
-  sp.temp.data.env <- list(sp_coords = years_env[,this.species$lon_lat_vars],
+  sp.temp.data.env <- list(sp_coords = years_env[,this_species$lon_lat_vars],
                            coords_env_data_all = years_env,
                            env_data_train = years_env[train_data,
                                                       4:(dim(years_env)[2]-1)],
@@ -47,8 +48,8 @@ extract_by_year <- function(this.species,layers_pattern){
                                                      4:(dim(years_env)[2]-1)],
                            test_data =years_env[-train_data,],
                            sp_occs_year = years_env$ID_YEAR,
-                           oocs_data = this.species$oocs_data,
-                           lon_lat_vars = this.species$lon_lat_vars,
+                           oocs_data = this_species$oocs_data,
+                           lon_lat_vars = this_species$lon_lat_vars,
                            layers_path_by_year = capasByRes)
   class(sp.temp.data.env) <- c("list", "sp.temporal.modeling","sp.temporal.env")
 
--- R/find_best_model.R
+++ R/find_best_model.R
@@ -1,22 +1,24 @@
 #' Function to find the best n-dimensional ellipsoid model using Partial Roc as a performance criteria.
-#' @param this.species, Species Temporal Environment "sp.temporal.env" object see \code{\link[hdm]{extract_by_year}}.
-#' @param cor_threshold Threshold valuefrom which it is considered that the correlation is high see \code{\link[hdm]{correlation_finder}}.
-#' @param ellipsoid_level The proportion of points to be included inside the ellipsoid see \code{\link[hdm]{ellipsoidfit}}.
+#' @param this_species, Species Temporal Environment "sp.temporal.env" object see \code{\link[hsi]{extract_by_year}}.
+#' @param cor_threshold Threshold valuefrom which it is considered that the correlation is high see \code{\link[hsi]{correlation_finder}}.
+#' @param ellipsoid_level The proportion of points to be included inside the ellipsoid see \code{\link[hsi]{ellipsoidfit}}.
 #' @param nvars_to_fit Number of variables that will be used to model.
-#' @param E  Amount of error admissible for Partial Roc test (by default =.05). Value should range between 0 - 1. see \code{\link[hdm]{PartialROC}}
-#' @param RandomPercent Occurrence points to be sampled in randomly for the boostrap of the Partial Roc test \code{\link[hdm]{PartialROC}}.
-#' @param NoOfIteration Number of iteration for the bootstrapping of the Partial Roc test \code{\link[hdm]{PartialROC}}.
+#' @param plot3d Logical. If the models  have 3 varibles an rgl plot will be shown
+#' @param E  Amount of error admissible for Partial Roc test (by default =.05). Value should range between 0 - 1. see \code{\link[hsi]{PartialROC}}
+#' @param RandomPercent Occurrence points to be sampled in randomly for the boostrap of the Partial Roc test \code{\link[hsi]{PartialROC}}.
+#' @param NoOfIteration Number of iteration for the bootstrapping of the Partial Roc test \code{\link[hsi]{PartialROC}}.
 #' @return A "sp.temp.best.model" object with metadata of the best model given the performance of the Partial Roc test.
 #' @export
 
-find_best_model <- function(this.species,cor_threshold=0.9,
+find_best_model <- function(this_species,cor_threshold=0.9,
                             ellipsoid_level=0.975,nvars_to_fit=3,
+                            plot3d=FALSE,
                             E = 0.05,
                             RandomPercent = 50,
                             NoOfIteration=1000){
-  stopifnot(inherits(this.species, "sp.temporal.env"))
-  n_nas <- floor(dim(this.species$env_data_train)[1]*0.1)
-  env_train <- this.species$env_data_train
+  stopifnot(inherits(this_species, "sp.temporal.env"))
+  n_nas <- floor(dim(this_species$env_data_train)[1]*0.1)
+  env_train <- this_species$env_data_train
 
   rm_layers <- unlist(sapply( 1:dim(env_train)[2], function(x){
     if(length(which(is.na(env_train[,x]))) > n_nas) return(x)
@@ -31,17 +33,20 @@ find_best_model <- function(this.species,cor_threshold=0.9,
   cor_filter <- find_cor$descriptors
   combinatoria_vars <- combn(length(cor_filter),nvars_to_fit)
 
-  year_to_search <- min(as.numeric(names(this.species$layers_path_by_year)))
+  year_to_search <- min(as.numeric(names(this_species$layers_path_by_year)))
+  cat("The total number of models to be tested are: ", dim(combinatoria_vars)[2],"...\n\n")
 
-  env_layers <- raster::stack(this.species$layers_path_by_year[[paste0(year_to_search)]])
+  env_layers <- raster::stack(this_species$layers_path_by_year[[paste0(year_to_search)]])
   modelos <- lapply(1:dim(combinatoria_vars)[2],function(x){
+    cat("Doing model: ", x," of ", dim(combinatoria_vars)[2],"\n")
+
     # Varaibles filtadas por combinatiria de las mas representativas
     vars_model <- cor_filter[combinatoria_vars[,x]]
     ellip <- cov_center(env_train[,vars_model],
                         level = ellipsoid_level ,vars = vars_model)
 
     # Datos de presencia de la sp en el ambiente
-    occs_env <- this.species$env_data_train[,vars_model]
+    occs_env <- this_species$env_data_train[,vars_model]
 
     # Ajuste del modelo de elipsoide
 
@@ -49,9 +54,10 @@ find_best_model <- function(this.species,cor_threshold=0.9,
                              centroid =ellip$centroid,
                              covar =  ellip$covariance,
                              level = ellipsoid_level,
-                             threshold = 0.000001,size = 3)
+                             threshold = 0.000001,size = 3,
+                             plot = plot3d)
 
-    if(length(ellip$centroid)==3){
+    if(length(ellip$centroid)==3 && plot3d){
       # Presencias de la sp en el ambiente
       rgl::points3d(occs_env,size=10)
 
@@ -75,7 +81,7 @@ find_best_model <- function(this.species,cor_threshold=0.9,
 
     }
 
-    valData <- this.species$test_data[,c(1,2)]
+    valData <- this_species$test_data[,c(1,2)]
     valData$sp_name <- "sp"
     valData <- valData[,c(3,1,2)]
     p_roc<- PartialROC(valData = valData,
@@ -86,7 +92,7 @@ find_best_model <- function(this.species,cor_threshold=0.9,
     p_roc$model <- paste0(x)
 
     return(list(model = sp_model$suitRaster,
-                pRoc=p_roc[,c("AUC_ratio","model")],
+                pRoc=p_roc[,c("auc_ratio","model")],
                 metadata=ellip))
 
   })
@@ -96,10 +102,10 @@ find_best_model <- function(this.species,cor_threshold=0.9,
   procs <- do.call("rbind.data.frame",procs)
   procs$model <- as.factor(procs$model)
 
-  m1 <- lm(AUC_ratio ~ model, data = procs)
+  m1 <- lm(auc_ratio ~ model, data = procs)
   model_means <- sapply(levels(procs$model), function(y){
     model_index <- which(procs$model == y)
-    media_model <- mean(procs[model_index,1])
+    media_model <- mean(procs[model_index,1],na.rm=T)
     return(media_model)
   })
 
@@ -111,15 +117,15 @@ find_best_model <- function(this.species,cor_threshold=0.9,
 
   best_model_metadata <- modelos[[as.numeric(best_model)]][[3]]
 
-  sp.temp.best.model <- list(sp_coords = this.species$sp_coords,
-                             coords_env_data_all = this.species$coords_env_data_all,
-                             env_data_train = this.species$env_data_train,
-                             env_data_test = this.species$env_data_test,
-                             test_data = this.species$test_data,
-                             sp_occs_year = this.species$sp_occs_year,
-                             oocs_data = this.species$oocs_data,
-                             lon_lat_vars = this.species$lon_lat_vars,
-                             layers_path_by_year = this.species$layers_path_by_year,
+  sp.temp.best.model <- list(sp_coords = this_species$sp_coords,
+                             coords_env_data_all = this_species$coords_env_data_all,
+                             env_data_train = this_species$env_data_train,
+                             env_data_test = this_species$env_data_test,
+                             test_data = this_species$test_data,
+                             sp_occs_year = this_species$sp_occs_year,
+                             oocs_data = this_species$oocs_data,
+                             lon_lat_vars = this_species$lon_lat_vars,
+                             layers_path_by_year = this_species$layers_path_by_year,
                              best_model_metadata= best_model_metadata,
                              ellipsoid_level =ellipsoid_level,
                              pROC_table = procs,
